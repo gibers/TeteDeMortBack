96d7141e2d53df3c6d7faadbb4cd8f7e89e91a4e
gibers <chbaptis@hotmail.fr>
2023-10-17 21:10:16 +0200

    first commit

---
+++ src/main/kotlin/com/example/demo/controllers/ConnectionController.kt
@@ -0,0 +1,28 @@
+package com.example.demo.controllers
+
+import mu.KotlinLogging
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@CrossOrigin
+@RestController
+class ConnectionController {
+
+    private val logger = KotlinLogging.logger {}
+
+    @PostMapping
+    fun doConnection(@RequestParam(name = "user") user: String, @RequestParam(name = "pass") pass: String): String {
+        logger.info { " user -> $user; pass -> $pass" }
+        return user;
+    }
+
+    @GetMapping(produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    fun doGet(): String {
+        return "it is OK";
+    }
+
+}
