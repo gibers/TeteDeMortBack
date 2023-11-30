31dffac305b98ca1616b41a4d7de0740760afe0c
gibers <chbaptis@hotmail.fr>
2023-11-30 22:52:33 +0100

    add file WebConfig.kt

---
+++ src/main/kotlin/com/example/demo/WebConfig.kt
@@ -0,0 +1,18 @@
+package com.example.demo
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://localhost")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+    }
+
+}
--- src/main/kotlin/com/example/demo/controllers/ConnectionController.kt
+++ src/main/kotlin/com/example/demo/controllers/ConnectionController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@CrossOrigin
+//@CrossOrigin
 @RestController
 class ConnectionController {
 
